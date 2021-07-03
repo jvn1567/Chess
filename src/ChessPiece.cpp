5be147937585caf86165d5514d20e90e5d48a145
John Nguyen <jvn1567@gmail.com>
2021-07-02 20:24:13 -0700

    initial commit

---
+++ src/ChessPiece.cpp
@@ -0,0 +1,10 @@
+#include "ChessPiece.h"
+
+unordered_map<int, int> ChessPiece::getMoves() const {
+    unordered_map<int, int> empty;
+    return empty;
+}
+
+string ChessPiece::getName() const {
+    return "ChessPiece";
+}
