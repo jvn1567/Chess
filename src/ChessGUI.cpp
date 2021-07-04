03e06354a3f02c504c367959e1baed3b14d0321f
John Nguyen <jvn1567@gmail.com>
2021-07-04 16:40:12 -0700

    refactored ChessGUI.cpp as main instead of class

--- src/ChessGUI.cpp
+++ src/ChessGUI.cpp
@@ -1,62 +1,30 @@
-#include "ChessGUI.h"
+#include "ChessBoard.h"
 #include <iomanip>
 #include <string>
-#include "gthread.h"
 #include "gwindow.h"
 #include "gbutton.h"
 #include "gevent.h"
 #include "glabel.h"
-#include "gobjects.h"
-#include "gradiobutton.h"
-#include "gtextfield.h"
+using namespace sgl;
+using namespace std;
 
-//mouse listener handlers don't work as member functions, prototyping here
-void processMouseEvent(GEvent mouseEvent);
-void selectPiece();
+static const int WINDOW_SIZE = 800; //may break if not divisible by 8
+static const int TILE_SIZE = WINDOW_SIZE / 8;
 
-//click handler function prototype
-actionFunc funcClick = nullptr;
+//global variables
+GWindow* window;
+ChessBoard* board;
+bool pieceIsSelected;
+int selectedRow;
+int selectedCol;
+unordered_map<int, int> movableTiles;
 
-ChessGUI::ChessGUI() {
-    //general initialization
-    window = new GWindow(WINDOW_SIZE + 200, WINDOW_SIZE); //offset for buttons
-    board = new ChessBoard();
-    selected = false;
-    board->setStartingBoard();
-    window->setLocation(300, 100);
-    window->setBackground("black");
-    window->setExitOnClose(true);
-    window->setAutoRepaint(false);
-    window->setTitle("Chess");
-    window->setMouseListener(processMouseEvent);
-    redraw();
-}
-
-ChessBoard* ChessGUI::getBoard() const {
-    return board;
-}
-
-bool ChessGUI::pieceIsSelected() const {
-    return selected;
-}
-
-void ChessGUI::redraw() {
-    for (int row = 0; row < 8; row ++) {
-        for (int col = 0; col < 8; col++) {
-            drawBackgroundTile(row, col);
-            drawTileHighlight(row, col);
-            drawPiece(row, col);
-        }
-    }
-    window->repaint();
-}
-
-void ChessGUI::fillTile(string color, int row, int col) {
+void fillTile(string color, int row, int col) {
     window->setColor(color);
     window->fillRect(col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE, TILE_SIZE);
 }
 
-void ChessGUI::drawBackgroundTile(int row, int col) {
+void drawBackgroundTile(int row, int col) {
     if ((row % 2 == 0) ^ (col % 2 == 0)) {
         fillTile("dark gray", row, col);
     } else {
@@ -64,8 +32,14 @@ void ChessGUI::drawBackgroundTile(int row, int col) {
     }
 }
 
-void ChessGUI::drawTileHighlight(int row, int col) {
-    if (selected) {
+void drawTileHighlight(int row, int col) {
+    if (pieceIsSelected) {
+        //highlight selected unit
+        if (row == selectedRow && col == selectedCol) {
+            cout << pieceIsSelected << endl;
+            fillTile("blue", row, col);
+        }
+        //highlight moveable tiles
         try {
             int col2 = movableTiles.at(row);
             bool movable = col == col2;
@@ -83,11 +57,12 @@ void ChessGUI::drawTileHighlight(int row, int col) {
             }
         } catch (exception ex) {
             //do nothing
+            (void)ex;
         }
     }
 }
 
-void ChessGUI::drawPiece(int row, int col) {
+void drawPiece(int row, int col) {
     ChessPiece* piece = board->getPiece(row, col);
     if (piece != nullptr) {
         string filename = "";
@@ -102,16 +77,59 @@ void ChessGUI::drawPiece(int row, int col) {
     }
 }
 
-void processMouseEvent(GEvent mouseEvent) {
+void redraw() {
+    for (int row = 0; row < 8; row ++) {
+        for (int col = 0; col < 8; col++) {
+            drawBackgroundTile(row, col);
+            drawTileHighlight(row, col);
+            drawPiece(row, col);
+        }
+    }
+    window->repaint();
+}
+
+void handleSelect(int row, int col) {
+    ChessPiece* piece = board->getPiece(row, col);
+    if (piece != nullptr && piece->isWhite()) {
+        selectedRow = row;
+        selectedCol = col;
+        pieceIsSelected = true;
+        movableTiles = board->getMoves(row, col);
+    redraw();
+    }
+}
+
+void handleMove(int row, int col) {
+    pieceIsSelected = false;
+    redraw();
+}
+
+void handleClick(GEvent mouseEvent) {
     int col = mouseEvent.getX() / TILE_SIZE;
     int row = mouseEvent.getY() / TILE_SIZE;
-    //funcClick(row, col);
+    if (pieceIsSelected) {
+        handleMove(row, col);
+    } else {
+        handleSelect(row, col);
+    }
 }
 
-void setClickFunction(actionFunc func) {
-    funcClick = func;
-}
-
-void selectPiece() {
-    
+int main() {
+    //general initialization
+    window = new GWindow(WINDOW_SIZE + 200, WINDOW_SIZE); //offset for buttons
+    board = new ChessBoard();
+    board->setStartingBoard();
+    pieceIsSelected = false;
+    selectedRow = 0;
+    selectedCol = 0;
+    window->setLocation(300, 100);
+    window->setBackground("black");
+    window->setExitOnClose(true);
+    window->setAutoRepaint(false);
+    window->setTitle("Chess");
+    window->setClickListener(handleClick);
+    cout << "check1" << endl;
+    redraw();
+    cout << "check2" << endl;
+    return 0;
 }
--- src/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "ChessGUI.h"
-
-int main() {
-    //testing
-    ChessGUI* gui = new ChessGUI();
-    return 0;
-}
